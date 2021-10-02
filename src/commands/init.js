6501e9f640c5b1915f6e3c229e5e068c5c58e15f
Zallow's Dev <super.conctacto@gmail.com>
2021-10-02 13:31:55 +0200

    (1/2) Roles System

--- src/commands/init.js
+++ src/commands/init.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
+const { MessageActionRow, MessageButton, MessageEmbed, MessageSelectMenu } = require('discord.js');
 
 const constants = require('../utils/constants');
 const { channels, roles } = require("../data/config.json");
@@ -14,6 +14,7 @@ module.exports = {
         if (await validPermissions(interaction, "ADMINISTRATOR") || await validRole(interaction, roles.staff)) {
             const verificationChannel = interaction.client.channels.cache.find(c => c.id === channels.verification);
             const ticketsChannel = interaction.client.channels.cache.find(c => c.id === channels.tickets);
+            const rolesChannel = interaction.client.channels.cache.find(c => c.id === channels.roles);
 
             const verificationButton = new MessageActionRow()
                 .addComponents(
@@ -23,7 +24,7 @@ module.exports = {
                         .setEmoji('✅')
                         .setStyle('SUCCESS')
                 );
-            
+
             const ticketsButton = new MessageActionRow()
                 .addComponents(
                     new MessageButton()
@@ -31,6 +32,66 @@ module.exports = {
                         .setLabel("Open Ticket")
                         .setEmoji('📩')
                         .setStyle('PRIMARY')
+                );
+
+            const rolesRow = new MessageActionRow()
+                .addComponents(
+                    new MessageSelectMenu()
+                        .setCustomId('roles')
+                        .setPlaceholder("No roles selected!")
+                        .setMinValues(1)
+                        .addOptions([
+                            {
+                                label: 'Ethical Hacker',
+                                value: 'ethical_hacker',
+                                emoji: '🔧'
+                            },
+                            {
+                                label: 'Pentester',
+                                value: 'pentester',
+                                emoji: '🪓'
+                            },
+                            {
+                                label: 'Cyber Security',
+                                value: 'cyber_security',
+                                emoji: '🔐'
+                            },
+                            {
+                                label: 'Game Dev',
+                                value: 'game_dev',
+                                emoji: '👾'
+                            },
+                            {
+                                label: 'Discord Bot Dev',
+                                value: 'discord_bot_dev',
+                                emoji: '⚙'
+                            },
+                            {
+                                label: 'Full-Stack',
+                                value: 'fullstack',
+                                emoji: '🥇'
+                            },
+                            {
+                                label: 'Back-End',
+                                value: 'backend',
+                                emoji: '👥'
+                            },
+                            {
+                                label: 'Front-End',
+                                value: 'frontend',
+                                emoji: '🎨'
+                            },
+                            {
+                                label: 'UX/UI',
+                                value: 'ux_ui',
+                                emoji: '📕'
+                            },
+                            {
+                                label: 'Graphic Designer',
+                                value: 'graphic_designer',
+                                emoji: '🎉'
+                            }
+                        ])
                 );
 
             const verificationEmbed = new MessageEmbed()
@@ -43,15 +104,22 @@ module.exports = {
                 .setTitle('Tickets')
                 .setDescription(':flag_es: Haz click abajo para abrir un ticket\n\n:flag_us: Click down below to open a Ticket');
 
+            const rolesEmbed = new MessageEmbed()
+                .setColor("#EE0000")
+                .setTitle("Roles")
+                .setDescription(":flag_es: Selecciona tus roles en dependencia de tus conocimientos\n\n:flag_us: Select your roles in dependance of your knowledge")
+
             const completedEmbed = new MessageEmbed()
                 .setColor("GREEN")
                 .setTitle(":white_check_mark: | Done!")
                 .setDescription("The embeds have been sent!")
-            
+
             await verificationChannel.bulkDelete(100);
-            await verificationChannel.send({ embeds: [verificationEmbed], components: [verificationButton]});
+            await verificationChannel.send({ embeds: [verificationEmbed], components: [verificationButton] });
             await ticketsChannel.bulkDelete(100);
-            await ticketsChannel.send({ embeds: [ticketsEmbed], components: [ticketsButton]});
+            await ticketsChannel.send({ embeds: [ticketsEmbed], components: [ticketsButton] });
+            await rolesChannel.bulkDelete(100);
+            await rolesChannel.send({ embeds: [rolesEmbed], components: [rolesRow] })
             await interaction.reply({ embeds: [completedEmbed], ephemeral: true });
         } else {
             interaction.reply({
