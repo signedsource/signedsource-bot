c6ead18334e61b5bb40613f7ca5babf71d13ec78
Zallow's Dev <super.conctacto@gmail.com>
2021-10-05 17:28:14 +0200

    Filtered the token again, but now regened it

--- src/handlers/activityHandler.js
+++ src/handlers/activityHandler.js
@@ -1,4 +1,4 @@
-const fetch = require("node-fetch");
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
 const activityHandler = async (client, channel, applicationId) => {
     return new Promise((res, rej) => {
