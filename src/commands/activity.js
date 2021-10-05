64c1e8ccceb2760e8c78debd77bd602b44849490
Zallow's Dev <super.conctacto@gmail.com>
2021-10-05 17:07:43 +0200

    Rename the property from channelTypes to channel_types

--- src/commands/activity.js
+++ src/commands/activity.js
@@ -30,7 +30,7 @@ module.exports = {
                 type: 7,
                 name: 'channel',
                 description: 'The voice channel that will be used for the activity',
-                channelTypes: ["GUILD_VOICE"],
+                channel_types: ["GUILD_VOICE"],
                 required: true
             },
             {
